0a68bf15189cc7b1f44e04abf6a72a7a32aece9a
mgroves <mgroves@telligent.com>
2011-02-20 12:05:36 -0500

    created LazyView aspect, removed the '.designer' file for MainActivity

---
+++ MonoStockPortfolio/Framework/LazyViewAttribute.cs
@@ -0,0 +1,30 @@
+using System;
+using Android.App;
+using Android.Util;
+using PostSharp.Aspects;
+
+namespace MonoStockPortfolio.Framework
+{
+    public class LazyViewAttribute : LocationInterceptionAspect
+    {
+        private readonly int _viewId;
+
+        public LazyViewAttribute(int viewId)
+        {
+            _viewId = viewId;
+        }
+
+        public override void OnGetValue(LocationInterceptionArgs args)
+        {
+            args.ProceedGetValue(); // this actually fetches the field and populates the args.Value
+            if (args.Value == null)
+            {
+                Log.Info("OnGetValue", "FindViewById called for " + args.LocationFullName);
+                var activity = args.Instance as Activity;
+                if (activity == null) throw new ArgumentException("LazyViewAttribute can only be used within Activities");
+                args.SetNewValue(activity.FindViewById(_viewId));
+                args.ProceedGetValue();
+            }
+        }
+    }
+}
