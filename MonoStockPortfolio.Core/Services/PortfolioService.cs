50fb96b5b5c52bebc0f79fd51b3ddfcc4a999021
mgroves <mgroves@telligent.com>
2011-04-01 23:30:58 -0400

    minor cleanup

--- MonoStockPortfolio.Core/Services/PortfolioService.cs
+++ MonoStockPortfolio.Core/Services/PortfolioService.cs
@@ -28,10 +28,11 @@ namespace MonoStockPortfolio.Core.Services
         {
             try
             {
+                var results = new List<PositionResultsViewModel>();
+
                 var positions = _portRepo.GetAllPositions(portfolioID);
                 if (!positions.Any()) return new List<PositionResultsViewModel>();
 
-                var results = new List<PositionResultsViewModel>();
                 var tickers = positions.Select(p => p.Ticker);
                 var stockData = _stockRepo.GetStockQuotes(tickers);
 
